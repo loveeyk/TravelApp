8478cd9c791d051d5b472924b9b27360949c0293
loveeyk <karir.loveleen@gmail.com>
2021-03-26 21:58:40 +0100

    created all components

--- TravelApp/src/app/app.module.ts
+++ TravelApp/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TravelComponent } from './travel/travel.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BlogComponent } from './blog/blog.component';
+import { HomeComponent } from './home/home.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TravelComponent,
+    NavbarComponent,
+    BlogComponent,
+    HomeComponent,
+    ContactusComponent,
+    AboutusComponent
   ],
   imports: [
     BrowserModule,
